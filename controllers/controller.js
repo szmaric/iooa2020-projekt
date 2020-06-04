76c850f2325526d7988594a8d9c8b9bf2713208f
Sanja <zmaric80@gmail.com>
2020-06-04 09:46:22 +0200

    login radi

--- controllers/controller.js
+++ controllers/controller.js
@@ -8,53 +8,33 @@ var bazaConfig = require('../config/baza');
 var connection = mysql.createConnection(bazaConfig.veza);
 
 
-//////////////////////PRIJAVA//////////////////////////////////////
-//async zapravo omogućava da server čeka završetak akcije tj. da se ne ugasi
 exports.prijava= (req,res)=>{
-    try {
-        const {email, password}=req.body;
-
-        if(!email || !password){
-            return res.status(400).render('prijava',{
-                message: 'Za prijavu je potrebno unijeti email i lozinku!'
+    const {email, password}=req.body;
+    connection.query('SELECT * FROM Admin WHERE email = ?',[email], (error,results, fields)=>{
+        if (error){
+            return res.render('prijava',{
+                message:'Upit nije dobro napisan!'
             });
-        }
-                                                                    //ovdje iza zareza ide async 
-        connection.query('SELECT * FROM Admin WHERE email = ?',[email], (error,results)=>{
-            console.log(results);
-                            //iza ! ide await, ali sa time mi nikako nije radio login, sada radi bez provjere lozinke  
-            if(!results || !(bcrypt.compare(password, results[0].password)) ){
-                res.status(401).render('prijava', {
-                    message: 'Korisničko ime ili lozinka nisu dobro unešeni!'
-                });
-            } 
-            else {
-                const sifra_admina = results[0].sifra_admina;
-
-                const token = jwt.sign({sifra_admina:sifra_admina}, process.env.JWT_SECRET, {
-                    expiresIn: process.env.JWT_EXPIRES_IN
-                });
-
-                console.log('Token je: '+token);
-
-                const cookieOptions = {
-                    expires: new Date(
-                        Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000 ),
-                  
-                };
-
-                res.cookie('jwt', token, cookieOptions);
-                res.status(200).redirect('/rezervacije');
+        }else{
+            if(results.length>0){
+                if(password==results[0].password){
+                    return res.status(200).render('rezervacije',{
+                        message2:'Uspješna prijava!'
+                    });
+                }else{
+                    return res.status(401).render('prijava',{
+                        message:'Email ili lozinka nisu dobro uneseni!'
+                    });
+                }
             }
-
-        });
-
-    } catch (error) {
-        console.log(error);
-    }
-
+            else{
+                return res.status(402).render('prijava',{
+                    message:'Korisnik sa unesenom email adresom ne postoji!'
+                });
+            }
+        }
+    });
 };
-
 
 
 
@@ -78,10 +58,8 @@ exports.registracija = (req, res)=>{
         });
        }
 
-       let hashedPassword = await  bcrypt.hash(password,8);
-       console.log(hashedPassword);
 
-       connection.query('INSERT INTO Admin SET ?', {username:username, password:hashedPassword, email:email},(error, results)=>{
+       connection.query('INSERT INTO Admin SET ?', {username:username, password:password, email:email},(error, results)=>{
             if(error){
                 console.log(error);
             }else{
--- misc/backup.js
+++ misc/backup.js
@@ -53,3 +53,53 @@ veza.connect((err) => {
   if (err) throw err;
   console.log('Uspješno povezivanje s bazom!');
 });*/
+
+
+
+//////////////////////PRIJAVA//////////////////////////////////////
+//async zapravo omogućava da server čeka završetak akcije tj. da se ne ugasi
+/*exports.prijava= (req,res)=>{
+    try {
+        const {email, password}=req.body;
+
+        if(!email || !password){
+            return res.status(400).render('prijava',{
+                message: 'Za prijavu je potrebno unijeti email i lozinku!'
+            });
+        }
+                                                                    //ovdje iza zareza ide async 
+        connection.query('SELECT * FROM Admin WHERE email = ?',[email], (error,results)=>{
+            console.log(results);
+                            //iza ! ide await, ali sa time mi nikako nije radio login, sada radi bez provjere lozinke  
+            if(!results.email || ! (bcrypt.compare(password, results[0].password))){
+                res.status(401).render('prijava', {
+                    message: 'Korisničko ime ili lozinka nisu dobro unešeni!'
+                }); 
+            }     
+            else {
+                const sifra_admina = results[0].sifra_admina;
+
+                const token = jwt.sign({sifra_admina:sifra_admina}, process.env.JWT_SECRET, {
+                    expiresIn: process.env.JWT_EXPIRES_IN
+                });
+
+                console.log('Token je: '+token);
+
+                const cookieOptions = {
+                    expires: new Date(
+                        Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000 ),
+                  
+                };
+
+                res.cookie('jwt', token, cookieOptions);
+                res.status(200).redirect('/rezervacije');
+            }
+
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+
+};*/
+
